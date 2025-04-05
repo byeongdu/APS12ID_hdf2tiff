d52ab68190c15c6a93e51a571f8c47e491ff6257
Lee <blee@anl.gov>
2025-04-05 11:18:49 -0500

    Update h5conv2tiff.m
    Image will be transposed.
    When filepath is not given, pwd will be used.

--- h5conv2tiff.m
+++ h5conv2tiff.m
@@ -1,7 +1,10 @@
 function h5conv2tiff(filename)
 entry = '/entry/data/data';
 R = h5read(filename, entry);
-%R = R';
+R = R';
 [filepath,name,~] = fileparts(filename);
+if isempty(filepath)
+    filepath = pwd;
+end
 filename = fullfile(filepath, filesep, [name, '.tif']);
 imwritetiff(R, filename, 32)
